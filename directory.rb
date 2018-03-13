5666f0a2dc95294cbbb506e186703823a2ad6dfb
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-03-13 19:19:13 +0000

    refactored for readability

--- directory.rb
+++ directory.rb
@@ -1,70 +1,76 @@
-# defining methods
+# define global variable for students
+@students = []
 
-def input_students
+# define methods
+def input_students # asks for names of students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
-def print_header
+def print_header # prints header
   puts "The students of Villans Academy"
   puts "-------------"
 end
 
-def print(students)
+def print_student_list(students) # prints list of students
   students.each_with_index { |student , i| puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
-def print_footer(names)
+def print_footer(names) # prints the footer with count of students
   puts "Overall, we have #{names.count} great students"
 end
 
-def no_students
+def no_students # message for user when no students are in the list
   puts "There are no names in the list yet!"
 end
 
-def interactive_menu
-  students = []
+def print_menu # prints the menu with user options
+  # show the menu to the user
+  puts "Please choose from the following options:"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students # shows the list of students with header/footer or an error if list is empty
+  if @students.empty?
+    no_students
+  else
+    print_header
+    print_student_list(@students)
+    print_footer(@students)
+  end
+end
+
+def interactive_menu # runs the menu
   loop {
-    # show the menu to the user
-    puts "Please choose from the following options:"
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     # save user input as a variable
     selection = gets.chomp
     # do what the user asked
     case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      if students.empty?
-        no_students
-      else
-        print_header
-        print(students)
-        print_footer(students)
-      end
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you meant, try again"
 
     end
-
    }
 
 end
